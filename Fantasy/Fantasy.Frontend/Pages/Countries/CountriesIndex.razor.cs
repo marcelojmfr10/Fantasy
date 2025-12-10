0a6d581c1b07cd6680df0b8968514f2eb927f4a6
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-12-09 18:56:57 -0600

    Add Generic List.

---
+++ Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,18 @@
+using Fantasy.Frontend.Repositories;
+using Fantasy.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Fantasy.Frontend.Pages.Countries
+{
+    public partial class CountriesIndex
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+        public List<Country>? Countries { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var responseHttp = await Repository.GetAsync<List<Country>>("api/countries");
+            Countries = responseHttp.Response;
+        }
+    }
+}
