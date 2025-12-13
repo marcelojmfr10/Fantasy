4156b1e350cfde60f21336c07d9ca85cb2423eed
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-12-13 09:27:54 -0600

    Multiple languages.

--- Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
+++ Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -1,11 +1,14 @@
 using Fantasy.Frontend.Repositories;
 using Fantasy.Shared.Entities;
+using Fantasy.Shared.Resources;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 
 namespace Fantasy.Frontend.Pages.Countries
 {
     public partial class CountriesIndex
     {
+        [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
         [Inject] private IRepository Repository { get; set; } = null!;
         public List<Country>? Countries { get; set; }
 
