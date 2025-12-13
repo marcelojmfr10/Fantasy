9ceff26c67e3a34ee83f88459361fe4d01db2d45
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-12-13 15:53:12 -0600

    Add SeedDb.

---
+++ Fantasy/Fantasy.Backend/Data/SeedDb.cs
@@ -0,0 +1,51 @@
+﻿
+using Fantasy.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fantasy.Backend.Data;
+
+public class SeedDb
+{
+    private readonly DataContext _context;
+
+    public SeedDb(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SeedAsync()
+    {
+        await _context.Database.EnsureCreatedAsync();
+        await CheckCountriesAsync();
+        await CheckTeamsAsync();
+    }
+
+    private async Task CheckCountriesAsync()
+    {
+        if (!_context.Countries.Any())
+        {
+            var countriesSQLScript = File.ReadAllText("Data\\Countries.sql");
+            await _context.Database.ExecuteSqlRawAsync(countriesSQLScript);
+        }
+    }
+
+    private async Task CheckTeamsAsync()
+    {
+        if (!_context.Teams.Any())
+        {
+            foreach (var country in _context.Countries)
+            {
+                _context.Teams.Add(new Team { Name = country.Name, Country = country! });
+                if (country.Name == "Argentina")
+                {
+                    _context.Teams.Add(new Team { Name = "Boca Juniors", Country = country! });
+                    _context.Teams.Add(new Team { Name = "River Plate", Country = country! });
+                    _context.Teams.Add(new Team { Name = "Argentinos Juniors", Country = country! });
+                    _context.Teams.Add(new Team { Name = "Tigre", Country = country! });
+                }
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- Fantasy/Fantasy.Shared/Entities/Country.cs
+++ Fantasy/Fantasy.Shared/Entities/Country.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fantasy.Shared.Resources;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -11,8 +12,9 @@ public class Country
 {
     public int Id { get; set; }
 
-    [MaxLength(100)]
-    [Required]
+    [Display(Name = "Country", ResourceType = typeof(Literals))]
+    [MaxLength(100, ErrorMessageResourceName = "MaxLength", ErrorMessageResourceType = typeof(Literals))]
+    [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Literals))]
     public string Name { get; set; } = null!;
 
     public ICollection<Team>? Teams { get; set; }
