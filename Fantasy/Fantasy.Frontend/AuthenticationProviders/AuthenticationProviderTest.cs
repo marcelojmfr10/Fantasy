37e25608ddf627ceaab6f655857032aab234f774
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-12-22 20:03:06 -0600

    Example of security system

---
+++ Fantasy/Fantasy.Frontend/AuthenticationProviders/AuthenticationProviderTest.cs
@@ -0,0 +1,26 @@
+﻿using Fantasy.Shared.Enums;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Fantasy.Frontend.AuthenticationProviders
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+            var user = new ClaimsIdentity(authenticationType: "test");
+
+            var admin = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName","Marcelo"),
+                new Claim("LastName","Fuentes"),
+                new Claim(ClaimTypes.Name, "chelo@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin"),
+            }, authenticationType: "test");
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(user)));
+        }
+    }
+}
