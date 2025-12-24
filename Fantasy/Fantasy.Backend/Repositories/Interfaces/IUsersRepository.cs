c85e85e7526167b61404a52728c4edfcd5a0f564
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-12-24 10:26:32 -0600

    Edit user and change password

--- Fantasy/Fantasy.Backend/Repositories/Interfaces/IUsersRepository.cs
+++ Fantasy/Fantasy.Backend/Repositories/Interfaces/IUsersRepository.cs
@@ -7,6 +7,8 @@ namespace Fantasy.Backend.Repositories.Interfaces;
 
 public interface IUsersRepository
 {
+    Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+    Task<IdentityResult> UpdateUserAsync(User user);
     Task<User> GetUserAsync(Guid userId);
     Task<string> GenerateEmailConfirmationTokenAsync(User user);
     Task<IdentityResult> ConfirmEmailAsync(User user, string token);
