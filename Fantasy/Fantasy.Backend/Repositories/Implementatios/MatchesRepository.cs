870a44deb1de492336413d59af7080ef4ad7fd02
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-12-31 10:52:01 -0600

    Close match

--- Fantasy/Fantasy.Backend/Repositories/Implementatios/MatchesRepository.cs
+++ Fantasy/Fantasy.Backend/Repositories/Implementatios/MatchesRepository.cs
@@ -3,6 +3,7 @@ using Fantasy.Backend.Helpers;
 using Fantasy.Backend.Repositories.Interfaces;
 using Fantasy.Shared.DTOs;
 using Fantasy.Shared.Entities;
+using Fantasy.Shared.Enums;
 using Fantasy.Shared.Responses;
 using Microsoft.EntityFrameworkCore;
 
@@ -201,10 +202,10 @@ public class MatchesRepository : GenericRepository<Match>, IMatchesRepository
         try
         {
             await _context.SaveChangesAsync();
-            //if (currentMatch.GoalsLocal != null && currentMatch.GoalsVisitor != null)
-            //{
-            //    await CloseMatchAsync(currentMatch);
-            //}
+            if (currentMatch.GoalsLocal != null && currentMatch.GoalsVisitor != null)
+            {
+                await CloseMatchAsync(currentMatch);
+            }
             return new ActionResponse<Match>
             {
                 WasSuccess = true,
@@ -221,45 +222,46 @@ public class MatchesRepository : GenericRepository<Match>, IMatchesRepository
         }
     }
 
-    //public async Task CloseMatchAsync(Match match)
-    //{
-    //    match.IsClosed = true;
-    //    _context.Update(match);
+    public async Task CloseMatchAsync(Match match)
+    {
+        match.IsClosed = true;
+        _context.Update(match);
 
-    //    var predictions = await _context.Predictions
-    //        .Where(x => x.MatchId == match.Id)
-    //        .ToListAsync();
-    //    foreach (var prediction in predictions)
-    //    {
-    //        var points = CalculatePoints(match, prediction);
-    //        prediction.Points = points;
-    //        _context.Update(prediction);
-    //    }
-    //    await _context.SaveChangesAsync();
-    //}
+        var predictions = await _context.Predictions
+            .Where(x => x.MatchId == match.Id)
+            .ToListAsync();
 
-    //public int CalculatePoints(Match match, Prediction prediction)
-    //{
-    //    int points = 0;
-    //    if (prediction.GoalsLocal == null || prediction.GoalsVisitor == null)
-    //    {
-    //        return points;
-    //    }
+        foreach (var prediction in predictions)
+        {
+            var points = CalculatePoints(match, prediction);
+            prediction.Points = points;
+            _context.Update(prediction);
+        }
+        await _context.SaveChangesAsync();
+    }
 
-    //    var matchStatus = GetMatchStatus(match.GoalsLocal!.Value, match.GoalsVisitor!.Value);
-    //    var predictionStatus = GetMatchStatus(prediction.GoalsLocal!.Value, prediction.GoalsVisitor!.Value);
-    //    if (matchStatus == predictionStatus) points += 5;
-    //    if (match.GoalsLocal == prediction.GoalsLocal) points += 2;
-    //    if (match.GoalsVisitor == prediction.GoalsVisitor) points += 2;
-    //    if (Math.Abs((decimal)match.GoalsLocal! - (decimal)match.GoalsVisitor!) == Math.Abs((decimal)prediction.GoalsLocal! - (decimal)prediction.GoalsVisitor!)) points++;
-    //    if (match.DoublePoints) points *= 2;
-    //    return points;
-    //}
+    public int CalculatePoints(Match match, Prediction prediction)
+    {
+        int points = 0;
+        if (prediction.GoalsLocal == null || prediction.GoalsVisitor == null)
+        {
+            return points;
+        }
 
-    //public MatchStatus GetMatchStatus(int goalsLocal, int goalsVisitor)
-    //{
-    //    if (goalsLocal > goalsVisitor) return MatchStatus.LocalWin;
-    //    if (goalsLocal < goalsVisitor) return MatchStatus.VisitorWin;
-    //    return MatchStatus.Tie;
-    //}
+        var matchStatus = GetMatchStatus(match.GoalsLocal!.Value, match.GoalsVisitor!.Value);
+        var predictionStatus = GetMatchStatus(prediction.GoalsLocal!.Value, prediction.GoalsVisitor!.Value);
+        if (matchStatus == predictionStatus) points += 5;
+        if (match.GoalsLocal == prediction.GoalsLocal) points += 2;
+        if (match.GoalsVisitor == prediction.GoalsVisitor) points += 2;
+        if (Math.Abs((decimal)match.GoalsLocal! - (decimal)match.GoalsVisitor!) == Math.Abs((decimal)prediction.GoalsLocal! - (decimal)prediction.GoalsVisitor!)) points++;
+        //if (match.DoublePoints) points *= 2;
+        return points;
+    }
+
+    public MatchStatus GetMatchStatus(int goalsLocal, int goalsVisitor)
+    {
+        if (goalsLocal > goalsVisitor) return MatchStatus.LocalWin;
+        if (goalsLocal < goalsVisitor) return MatchStatus.VisitorWin;
+        return MatchStatus.Tie;
+    }
 }
